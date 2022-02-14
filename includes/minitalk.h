e55a93c7976b6cb193c6a98ad86c70e1bdad2e36
gsemerar <gsemerar@student.42roma.it>
2022-02-14 14:20:44 +0100

    Inizializzazione repository

---
+++ includes/minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsemerar <gsemerar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/14 11:40:44 by gsemerar          #+#    #+#             */
+/*   Updated: 2022/02/14 11:43:35 by gsemerar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include "../ft_printf/includes/ft_printf.h"
+
+#endif
